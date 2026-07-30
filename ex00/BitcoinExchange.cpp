535992754ef77009c77fdc38c50002a3144f6a1d
Gautier Ducurtil <gaducurt@student.42.fr>
2026-07-30 15:29:00 +0200

    begin of ex00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,20 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/07 17:26:17 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/07/28 15:42:45 by gaducurt         ###   ########.fr       */
+/*   Created: 2026/07/29 14:45:08 by gaducurt          #+#    #+#             */
+/*   Updated: 2026/07/29 15:26:40 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int	main(int ac, char** av)
+BitcoinExchange::BitcoinExchange()
 {
-	if (ac != 2)
-		return (1);	
-	(void)av;
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &obj)
+{
+    
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange &obj)
+{
+    
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+    
 }
--- ex00/include/BitcoinExchange.hpp
+++ ex00/include/BitcoinExchange.hpp
@@ -6,7 +6,22 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 17:26:24 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/07/07 17:26:25 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/07/29 15:24:23 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef __BITCOIN__
+#define __BITCOIN__
+
+class BitcoinExchange
+{
+	private:
+
+    public:
+        BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange &obj);
+        BitcoinExchange &operator=(const BitcoinExchange &obj);
+        ~BitcoinExchange();
+};
+
+#endif
