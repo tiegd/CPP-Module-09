cd34a32dc8960bc9a3f1b9fe29c5353ff849d4ae
Gautier Ducurtil <gaducurt@student.42.fr>
2026-08-10 15:44:58 +0200

    ex02: begin

---
+++ ex02/include/PmergeMe.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 11:19:16 by gaducurt          #+#    #+#             */
+/*   Updated: 2026/08/10 14:59:58 by gaducurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __PEMERGEME__
+#define __PEMERGEME__
+
+#include <string>
+#include <vector>
+
+class PmergeMe
+{
+	private:
+		int*				_intArray;
+		double				_timeManagement;
+		double				_time1;
+		double				_time2;
+		std::string			_input;
+		std::vector<int>	_cont1;
+		// std::autre			_cont2; 
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe &obj);
+		PmergeMe &operator=(const PmergeMe &obj);
+		~PmergeMe();
+		PmergeMe(std::string include);
+		void	mergeInset();
+		void	sort1(); // change the name after chosing both containers.
+		void	sort2(); // change the name after chosing both containers.
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 11:19:12 by gaducurt          #+#    #+#             */
+/*   Updated: 2026/08/10 14:39:59 by gaducurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+#include <iostream>
+
+int	main(int ac, char** av)
+{
+	if (ac < 2)
+		return 1;
+	else
+	{
+		try
+		{
+			PmergeMe	paul;
+			paul.mergeInset();
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	return 0;
+}
